37c16985c982426a34eaa7842153e3f2a517eab1
titoycute <titoycustodio@gmail.com>
2026-03-27 14:04:40 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'pokesim-v1';
+const assets = [
+  'index.html',
+  'manifest.json',
+  'favicon.ico',
+  'PokeSim_Pokemon.png',
+  'PokeSim Word.png',
+  'PokeSim_Icon.png',
+  'music1.mp3',
+  'music2.mp3'
+];
+
+// Install Service Worker
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Fetch logic
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
